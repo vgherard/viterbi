a5a0eaa3e558df66becdb36a6880db4bede14044
vgherard <vgherard@sissa.it>
2021-01-12 12:00:06 +0100

    No need to expose viterbi::forward_pass() and viterbi::backward_pass()

--- viterbi.h
+++ viterbi.h
@@ -5,22 +5,6 @@
 #include "Matrix.h"
 
 namespace viterbi {
-        void initialize(
-                Matrix<double> &,
-                Matrix<size_t> &, 
-                size_t, 
-                const Matrix<double> &, 
-                const std::vector<double> &
-        );
-
-        void forward_pass(
-                Matrix<double> &,
-                Matrix<size_t> &,
-                const std::vector<size_t> &,
-                const Matrix<double> &, 
-                const Matrix<double> &
-        ); 
-        
         std::vector<size_t> optimal_path(
                         const std::vector<size_t> &,
                         const Matrix<double> &,
